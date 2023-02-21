23e554e2cb178e09b44c0687ae1ae88a79ecd0c6
rachmaninoff <litakmnz@gmail.com>
2023-02-21 21:30:53 +0800

    first

---
+++ test.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Property{
+		uint public price;
+        string location;
+        address public immutable owner;
+		
+        constructor(string memory _location){
+            location = _location;
+            owner = msg.sender;
+        }
+
+
+		function getPrice() public view returns(uint){
+				return price;
+		}
+
+        
+}
